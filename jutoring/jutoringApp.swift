36b61ef195c7633ae7682c2fe11ec5d21b971670
Jason Ngo <jason.n.ngo@gmail.com>
2025-02-12 18:31:37 -0500

    Initial Commit

---
+++ jutoring/jutoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  jutoringApp.swift
+//  jutoring
+//
+//  Created by Jason Ngo on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct jutoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jutoringTests/jutoringTests.swift
@@ -0,0 +1,17 @@
+//
+//  jutoringTests.swift
+//  jutoringTests
+//
+//  Created by Jason Ngo on 2025-02-12.
+//
+
+import Testing
+@testable import jutoring
+
+struct jutoringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
